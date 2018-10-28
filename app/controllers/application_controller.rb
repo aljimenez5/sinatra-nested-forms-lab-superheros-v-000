13f59ec685071b7abbc36966c33118d5d8bd242f
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-10-28 03:29:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,6 @@ class App < Sinatra::Base
   
   post '/teams' do
     @team = Team.new(params[:team])
-    binding.pry
     params[:team][:members].each do |member|
       Hero.new(member)
     end
