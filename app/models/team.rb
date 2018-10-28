c8e6174487ba15fe5d04b61b12f6a81755cceec7
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-10-28 02:54:26 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,15 @@
 class Team 
+  attr_accessor :name, :motto
+  HEROES = []
   
+  def initialize(params)
+    @name = name
+    @power = motto 
+    HEROES << self
+  end
+  
+  def self.all
+    HEROES
+  end  
   
 end
